bd45aa37eb5ec2662e0d4d9a02fad46bd7d207f6
thedannysoto <87dsoto@gmail.com>
2020-07-01 21:54:01 -0500

    Connects 'lower' and 'higher' buttons on game screen to nextGuessHandler function to give the next guess, also adds validation in case user presses wrong button

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
@@ -7,7 +7,7 @@ import Card from '../components/Card';
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
     max = Math.floor(max);
-    const rndNum = Math.floor(Math.random() * (max - min) + min);
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
     if (rndNum === exclude) {
         return generateRandomBetween(min, max, exclude);
     } else {
@@ -18,13 +18,38 @@ const generateRandomBetween = (min, max, exclude) => {
 const GameScreen = props => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
 
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+
+    const nextGuessHandler = direction => {
+        if (
+            (direction === 'lower' && currentGuess < props.userChoice) || 
+            (direction === 'higher' && currentGuess > props.userChoice)
+            ) {
+            Alert.alert('Don\'t Lie!', 'Please give the correct hints.', [
+                {text: 'Sorry!', style: 'cancel'}
+            ]);
+            return;
+        }
+        if (direction === 'lower') {
+            currentHigh.current = currentGuess;
+        } else {
+            currentLow.current = currentGuess;
+        }
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+        console.log("High", currentHigh.current);
+        console.log("Low", currentLow.current);
+        setCurrentGuess(nextNumber);
+    }
+
+
     return(
         <View style={styles.screen}>
             <Text>Opponent's Guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title="LOWER" onPress={() => {}} />
-                <Button title="HIGHER" onPress={() => {}} />
+                <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
+                <Button title="HIGHER" onPress={nextGuessHandler.bind(this, 'higher')} />
             </Card>
         </View>
     )
